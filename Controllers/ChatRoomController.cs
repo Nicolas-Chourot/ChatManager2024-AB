6d509f3c5a6f83080c22e534c91484e9bbf6bbc3
Nicolas Chourot <Nicolas.Chourot@clg.qc.ca>
2024-04-18 13:42:25 -0400

    Ajoutez des fichiers projet.

---
+++ Controllers/ChatRoomController.cs
@@ -0,0 +1,20 @@
+﻿using ChatManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChatManager.Controllers
+{
+    [OnlineUsers.UserAccess]
+    public class ChatRoomController : Controller
+    {
+        // GET: ChatRoom
+        public ActionResult Index()
+        {
+            Session["LastAction"] = "/Accounts/GroupEmail";
+            return View();
+        }
+    }
+}
